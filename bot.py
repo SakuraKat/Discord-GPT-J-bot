3742fe922c62db5180b88493b811dc00bb754694
SakuraKat <SakuraKat1F338@gmail.com>
2022-12-05 23:30:27 +0530

    combined file from the template

--- bot.py
+++ bot.py
@@ -1,22 +1,182 @@
 # Path: bot.py
-# Write a discord bot that can do the following:
-# 1. When a user types something in a channel, the bot will log it to console
-# Use slash commands to make the bot do the following:
-# 1. When a user types /help, the bot will send a message to the channel with the help message
-# 2. When a user types /ping, the bot will send a message to the channel with the ping
-# 3. When a user types /echo, the bot will send a message to the channel with the message that the user typed
+# Using template: https://github.com/kkrypt0nn/Python-Discord-Bot-Template
+#
 
-# All the imports required in single line
+import asyncio
+import json
+import os
+import platform
+import random
+import sys
+
+import aiosqlite
 import discord
+from discord.ext import commands, tasks
+from discord.ext.commands import Bot, Context
+
+from data import exceptions
+
+if not os.path.isfile("config.json"):
+    sys.exit("'config.json' not found! Please add it and try again.")
+else:
+    with open("config.json") as file:
+        config = json.load(file)
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = Bot(command_prefix=commands.when_mentioned_or(
+    config["prefix"]), intents=intents, help_command=None)
 
 
-# Bot code
-async def on_message(message):
-    print(message.content)
+async def init_db():
+    async with aiosqlite.connect("data/database/database.db") as db:
+        with open("data/database/schema.sql") as file:
+            await db.executescript(file.read())
+        await db.commit()
 
 
+bot.config = config
 
 
-class Bot(discord.Client):
-    async def on_ready(self):
-        print(f"Logged in as {self.user}")
+@bot.event
+async def on_ready() -> None:
+    """
+    The code in this even is executed when the bot is ready
+    """
+    print(f"Logged in as {bot.user.name}")
+    print(f"discord.py API version: {discord.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
+    status_task.start()
+    if config["sync_commands_globally"]:
+        print("Syncing commands globally...")
+        await bot.tree.sync()
+
+
+@tasks.loop(minutes=1.0)
+async def status_task() -> None:
+    """
+    Setup the game status task of the bot
+    """
+    statuses = ["with you!", "with Krypton!", "with humans!"]
+    await bot.change_presence(activity=discord.Game(random.choice(statuses)))
+
+
+@bot.event
+async def on_message(message: discord.Message) -> None:
+    """
+    The code in this event is executed every time someone sends a message, with or without the prefix
+
+    :param message: The message that was sent.
+    """
+    if message.author == bot.user or message.author.bot:
+        return
+
+    if not message.content.startswith(config["prefix"]):
+        await do_magic(message)
+    await bot.process_commands(message)
+
+
+@bot.event
+async def on_command_completion(context: Context) -> None:
+    """
+    The code in this event is executed every time a normal command has been *successfully* executed
+    :param context: The context of the command that has been executed.
+    """
+    full_command_name = context.command.qualified_name
+    split = full_command_name.split(" ")
+    executed_command = str(split[0])
+    if context.guild is not None:
+        print(
+            f"Executed {executed_command} command in {context.guild.name} (ID: {context.guild.id}) by {context.author} (ID: {context.author.id})")
+    else:
+        print(
+            f"Executed {executed_command} command by {context.author} (ID: {context.author.id}) in DMs")
+
+
+@bot.event
+async def on_command_error(context: Context, error) -> None:
+    """
+    The code in this event is executed every time a normal valid command catches an error
+    :param context: The context of the normal command that failed executing.
+    :param error: The error that has been faced.
+    """
+    if isinstance(error, commands.CommandOnCooldown):
+        minutes, seconds = divmod(error.retry_after, 60)
+        hours, minutes = divmod(minutes, 60)
+        hours = hours % 24
+        embed = discord.Embed(
+            title="Hey, please slow down!",
+            description=f"You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, exceptions.UserBlacklisted):
+        """
+        The code here will only execute if the error is an instance of 'UserBlacklisted', which can occur when using
+        the @checks.not_blacklisted() check in your command, or you can raise the error by yourself.
+        """
+        embed = discord.Embed(
+            title="Error!",
+            description="You are blacklisted from using the bot.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, exceptions.UserNotOwner):
+        """
+        Same as above, just for the @checks.is_owner() check.
+        """
+        embed = discord.Embed(
+            title="Error!",
+            description="You are not the owner of the bot!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingPermissions):
+        embed = discord.Embed(
+            title="Error!",
+            description="You are missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to execute this command!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = discord.Embed(
+            title="Error!",
+            # We need to capitalize because the command arguments have no capital letter in the code.
+            description=str(error).capitalize(),
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    raise error
+
+
+async def load_cogs() -> None:
+    """
+    The code in this function is executed whenever the bot will start.
+    """
+    for file in os.listdir(f"./data/cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                await bot.load_extension(f"data.cogs.{extension}")
+                print(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
+
+
+async def do_magic(message: discord.Message):
+    """
+    The code in this function is executed every time someone sends a message without the prefix.
+    """
+    if message.content == "Hello":
+        await message.reply("Hello!")
+
+
+if __name__ == "__main__":
+    asyncio.run(init_db())
+    asyncio.run(load_cogs())
+    bot.run(config["token"])
